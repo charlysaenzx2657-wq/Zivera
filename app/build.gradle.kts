ea4bb70ce808921f51967766f61101766c63059f
Carlos Saenzx <charlysaenzx2657@gmail.com>
2026-09-20 20:42:31 -0600

    Update build.gradle.kts for Compose configuration
    Removed the Compose plugin and added compose options with a specific Kotlin compiler extension version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
 
@@ -51,6 +50,10 @@ android {
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     packaging {
